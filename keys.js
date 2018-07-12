a3f824b5b1a68e95dfc4e205c7b8e13f57568bb4
Erin0418 <erinf0418@gmail.com>
2018-07-11 22:33:15 -0400

    npm and apis added

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.aiv2KeUmkv25H0Vj6bIKjClTF,
+  consumer_secret: process.env.eXKoflTOpFAPCft8vNARdHW2GTaShCVPCdq9hcLes7AhAI9CnL,
+  access_token_key: process.env.WA5rq8ZZ8G8nzOU7HVv8oZJZzvFeyA,
+  access_token_secret: process.env.gADXdrRqesYWH1O89Gslt5DIyv5MNJv3abGWwFY6W5sxW
 };
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.c0bd90e2359e4acf90bd658445144f53,
+  secret: process.env.a7b9bd8994cd4d118235d292158d4868,
 };
